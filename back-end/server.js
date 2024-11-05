b7d9f58426c32599609a16fe1ad8819bc52a04f8
donia <belhoulaturki.donia@gmail.com>
2024-11-05 13:03:32 +0100

    definir les endpoints inscription et connexion

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,27 @@
 const express = require("express"); //importer express: le serveur tourne tout le temps: en recherche active
-const app = express(); //initialiser l'application: la methode  express
+
 const mysql = require("mysql2"); // Pour se connecter à MySQL
-const port = 3000; //definir le port du backend
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const bodyparser = require("body-parser");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const app = express(); //initialiser l'application: la methode  express
+
+const port = 3000; //definir le port du backend
+
 app.use(cors()); // autorise toutes les origines à accéder aux endpoints
+const uri =
+  "mongodb+srv://belhoulaturkidonia:Dandoun1983@clusterfrigo.ebifl.mongodb.net/?retryWrites=true&w=majority&appName=ClusterFrigo";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!"); //hello word est envoyé dans un consolelog et pas en response
@@ -32,13 +50,13 @@ db.connect((err) => {
 });
 
 // Exécutez: effectuez une requête SQL puisque je suis connecte à ma BDD
-db.query("SELECT * FROM produits", (err, results) => {
+/*db.query("SELECT * FROM produits", (err, results) => {
   if (err) {
     console.error("Erreur lors de la requête SQL :", err);
     return;
   }
   console.log("Résultats :", results);
-});
+});*/
 
 // Fermez la connexion après l'exécution
 //db.end();
@@ -101,39 +119,155 @@ HAVING COUNT(ingredients.id) = (SELECT COUNT(*) FROM ingredients WHERE recette_i
 });*/
 
 // Route POST pour ajouter un produit
-app.post('/ajouterproduit', (req, res) => {
-  const { nom, quantite, date_expiration, categorie } = req.body;//on definie un objet avec 4 variables et attribue a chaque variable un champ de body de requete
+app.post("/ajouterproduit", (req, res) => {
+  const { nom, quantite, date_expiration, categorie } = req.body; //on definie un objet avec 4 variables et attribue a chaque variable un champ de body de requete
   console.log(nom, date_expiration, categorie);
 
-    // const sql =`INSERT INTO produits (nom, quantite, date_expiration, categorie) VALUES (?, ?, ?, ?)`
-    // db.query(sql, [nom, quantity, datedexpi, category],
+  // const sql =`INSERT INTO produits (nom, quantite, date_expiration, categorie) VALUES (?, ?, ?, ?)`
+  // db.query(sql, [nom, quantity, datedexpi, category],
 
-    db.query(`INSERT INTO produits (nom, quantite, date_expiration, categorie)
+  db.query(
+    `INSERT INTO produits (nom, quantite, date_expiration, categorie)
       VALUES
-      ('${nom}', '${quantite}', '${date_expiration}', '${categorie}')`, 
-      function (err, results, fields) {
-        console.log("Resultats", results, err, fields);
-        res.send('Post request to ajouter produit');
-      }
-    );
-
-});
+      ('${nom}', '${quantite}', '${date_expiration}', '${categorie}')`,
+    function (err, results, fields) {
+      console.log("Resultats", results, err, fields);
+      res.send("Post request to ajouter produit");
+    }
+  );});
 
 // Route POST pour ajouter une recette
-app.post('/ajouterrecette', (req, res) => {
-  const { nom, instructions, difficulte, temps_preparation, ingredients } = req.body;//on definie un objet avec 4 variables et attribue a chaque variable un champ de body de requete
+app.post("/ajouterrecette", (req, res) => {
+  const { nom, instructions, difficulte, temps_preparation, ingredients } =
+    req.body; //on definie un objet avec 4 variables et attribue a chaque variable un champ de body de requete
   console.log(nom, difficulte, temps_preparation);
 
-    // const sql =`INSERT INTO produits (nom, quantite, date_expiration, categorie) VALUES (?, ?, ?, ?)`
-    // db.query(sql, [nom, quantity, datedexpi, category],
+  // const sql =`INSERT INTO produits (nom, quantite, date_expiration, categorie) VALUES (?, ?, ?, ?)`
+  // db.query(sql, [nom, quantity, datedexpi, category],
 
-    db.query(`INSERT INTO recettes (nom, instructions, difficulte, temps_preparation, ingredients)
+  db.query(
+    `INSERT INTO recettes (nom, instructions, difficulte, temps_preparation, ingredients)
       VALUES
-      ('${nom}', '${instructions}', '${difficulte}', '${temps_preparation}', '${ingredients}')`, 
-      function (err, results, fields) {
-        console.log("Resultats", results, err, fields);
-        res.send('Post request to ajouter recette');
-      }
+      ('${nom}', '${instructions}', '${difficulte}', '${temps_preparation}', '${ingredients}')`,
+    function (err, results, fields) {
+      console.log("Resultats", results, err, fields);
+      res.send("Post request to ajouter recette");
+    }
+  );});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
     );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();  il faut mettre en commentaire le close connection car jai besoin de se connecter a ma BDD
+    AfficherMesCollections();
+    //SuppressionUserInsteadOne();
+    GetIfOfAnUser("sean_bean@gameofthron.es");
+
+  }
+}
+
+
+ function AfficherMesCollections(){
+  const dbName = "sample_mflix";
+  const db = client.db(dbName);
+  db.listCollections()
+    .toArray()
+    .then((cols)=>
+      cols.map((col, index) => {
+        console.log(`Collections ${index} :`,col)
+    })
+  );
+}
+
+/*async function SuppUserInsteadOne(){
+  const dbName = "sample_mflix";
+  const db = client.db(dbName);
+  const usersCollection = await db.collection('users');
+  const req = {_id: {$ne: new ObjectId("59b99db4cfa9a34dcd7885b6")}}
+  const result = await usersCollection.deleteMany(req);
+  console.log(result);
+}
+
+async function SuppressionUserInsteadOne(){
+  const db = SelectGoodDB("sample_mflix");
+  const usersCollection = db.collection('users');
+  const req = { _id: { $ne: new ObjectId("59b99db4cfa9a34dcd7885b6") } }
+  //const result = await usersCollection.deleteMany(req);
+  console.log(result)
+}*/
+
+
+// ALTER les deux tables produits et ingredients pour ajouter une colonne idUser
+    // ça se fait dans le SQL shell
+// Modifier les points d'api pour qui prennent un id d'utilisateur (back)
+// Modifier les appels front pour que les requêtes passes un id d'utilisateurs (front)
+// Crée en front un formulaire pour la connexion et un formulaire l'inscription (front)
+// Crée deux endpoint login et un endpoint inscription (back)
+// Crée un endpoint qui récupère l'utilisateur actuel (back)
+
+function SelectGoodDB(dbName) { 
+  return client.db(dbName);
+}
+
+async function GetIfOfAnUser(emailOfUser) {
+  const db = SelectGoodDB("sample_mflix")
+  const usersCollection = db.collection('users');
+  const req = { email : emailOfUser}
+  const result = await usersCollection.findOne(req)
+  console.log("MonUser", result._id)
+  return result._id
+}
+
+
+app.post("/inscription", async (req, res)=> {
+  const {name, email, password} = req.body;
+  console.log("req body", req.body)
+  const db = SelectGoodDB("sample_mflix")
+  const usersCollection = db.collection('users');
+  const passwordHashed = await bcrypt.hash(password, saltRounds);
+  const result = await usersCollection.insertOne({name, email, passwordHashed })
+  console.log(result)
+  return res.send("Utilisateur crée avec succès")
+})
+
+
+run().catch(console.dir);
+
+//endpoint de  connection 
+//post ou get à définir
+//je recupere ce que j'ai dans le body
+//je fais une requete mongodb pour voir si y'a bien un email qui existe 
+//ensuite vu que j'ai fait une requete en amont, j'en profite pour
+//recuperer le mot de passe hashé 
+//j'utilise bcrypt pour voir si le mot de passe que j'ai recu en clair
+//est la meme que celui qui est haché
+//je renvoie à l'utilisteur soit connexion réussi soit connexion échoué  
+
+
+app.post("/connexion", async (req, res)=> {//on ecoute les requetes post qui est sur la route connecxion
+  const { email, password} = req.body;   // on lit le body et on stocke email et pass dans une constante
+  console.log("req body", req.body)
+  const db = SelectGoodDB("sample_mflix"); 
+  const usersCollection = db.collection('users');      //on selectionne la collection users
+  const utilisateurFind = await usersCollection.findOne({email});
+  
+  if (utilisateurFind) {
+    const passwordHashedTrueOrNot = await bcrypt.compare(
+      password, 
+      utilisateurFind.passwordHashed
+    );
+    
+   return res.send(passwordHashedTrueOrNot ? "Connecté avec succés " :  "le mot de passe n'existe pas")
+  } else {
+    console.log("utilisateur n'existe pas")
+  }
 
 });
